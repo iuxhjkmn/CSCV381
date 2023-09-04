13287134e3c1f5e78ae2ca789fb399cb1d310002
iuxhjkmn <122479039+iuxhjkmn@users.noreply.github.com>
2023-09-03 19:08:11 -0500

    assignment1

---
+++ hellodate/app/src/main/java/edu/arizona/cast/kangzheng/hellodate/MainActivity.kt
@@ -0,0 +1,36 @@
+package edu.arizona.cast.kangzheng.hellodate
+//Import libraries
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var datebutton: Button//create variable for button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        datebutton=findViewById(R.id.show_time)//
+        datebutton.setOnClickListener {//create a listener to run the
+            //function when user clicks
+            (displayDate())//call the function
+        }
+    }
+
+    private fun displayDate() {//function created to run the code to get current
+        //time
+        val dateTime = LocalDateTime.now()
+        datebutton.text =
+            dateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL)
+                .withZone(
+                    ZoneId.systemDefault()))
+
+    }
+
+
+
+
+}
